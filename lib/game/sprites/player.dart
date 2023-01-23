e0435d80806fee26b824d684cbeb5f8ec1bad0a8
Anuj Panwar <anuj.kumar.panwar@maersk.com>
2023-01-23 18:15:21 +0530

    Camera, collision and speed level changes

--- lib/game/sprites/player.dart
+++ lib/game/sprites/player.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 
+import 'package:doodledash/game/sprites/platform.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/services.dart';
@@ -41,12 +42,14 @@ class Player extends SpriteGroupComponent<PlayerState>
   Character character;
   double jumpSpeed;
   // Core gameplay: Add _gravity property
+  final double _gravity = 9;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     // Core gameplay: Add circle hitbox to Dash
+    await add(CircleHitbox());
 
     // Add a Player to the game: loadCharacterSprites
     await _loadCharacterSprites();
@@ -74,6 +77,7 @@ class Player extends SpriteGroupComponent<PlayerState>
     }
 
     // Core gameplay: Add gravity
+    _velocity.y += _gravity;
 
     // Add a Player to the game: Calculate Dash's current position based on
     // her velocity over elapsed time since last update cycle
@@ -130,8 +134,25 @@ class Player extends SpriteGroupComponent<PlayerState>
   // Powerups: Add isWearingHat getter
 
   // Core gameplay: Override onCollision callback
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    bool isCollidingVertically =
+        (intersectionPoints.first.y - intersectionPoints.last.y).abs() < 5;
+
+    if (isMovingDown && isCollidingVertically) {
+      current = PlayerState.center;
+      if (other is NormalPlatform) {
+        jump();
+        return;
+      }
+    }
+  }
 
   // Core gameplay: Add a jump method
+  void jump({double? specialJumpSpeed}) {
+    _velocity.y = specialJumpSpeed != null ? -specialJumpSpeed : -jumpSpeed;
+  }
 
   void _removePowerupAfterTime(int ms) {
     Future.delayed(Duration(milliseconds: ms), () {
